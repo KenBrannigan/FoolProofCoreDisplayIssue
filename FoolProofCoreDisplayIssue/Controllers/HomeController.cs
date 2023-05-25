6fb6c4881ffaaafc663176de4f2311ab5c8de99a
Ken Brannigan <ken@fewwillhunt.com>
2023-05-25 10:18:55 -0400

    Add project files.

---
+++ FoolProofCoreDisplayIssue/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoolProofCoreDisplayIssue.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Models.Index());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Models.Index index)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Success");
+            }
+
+            return View(index);
+        }
+    }
+}
---
+++ FoolProofCoreDisplayIssue/Models/Index.cs
@@ -0,0 +1,14 @@
+﻿using FoolProof.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoolProofCoreDisplayIssue.Models
+{
+    public class Index
+    {
+        [Display(Name = "Goal Amount")]
+        [RequiredIf("IsRequired", true, ErrorMessage = "{0} is required.")]
+        public string? GoalAmount { get; set; }
+
+        public bool IsRequired { get; set; } = true;
+    }
+}
