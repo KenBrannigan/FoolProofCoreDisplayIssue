cfd8ebf820de05a1ed6e6855178682527b2a5255
Ken Brannigan <ken@fewwillhunt.com>
2023-05-25 20:45:52 -0400

    Added AddFoolProof call in Program.cs

--- FoolProofCoreDisplayIssue/Program.cs
+++ FoolProofCoreDisplayIssue/Program.cs
@@ -1,3 +1,5 @@
+using FoolProof.Core;
+
 namespace FoolProofCoreDisplayIssue
 {
     public class Program
@@ -8,6 +10,8 @@ namespace FoolProofCoreDisplayIssue
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddFoolProof();
 
             var app = builder.Build();
 
